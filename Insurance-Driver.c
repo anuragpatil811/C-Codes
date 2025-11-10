0feaff00806692dd72a82e8a90657a573f7e810b
Anurag Arun patil <anuragpatil811@gmail.com>
2025-11-10 10:09:53 +0530

    Add files via upload

---
+++ Insurance-Driver.c
@@ -0,0 +1,18 @@
+//Insurance of driver using logical operators
+#include<stdio.h>
+int main()
+{
+    char sex, ms;
+    int age;
+    printf("Enter age, sex, and marital status:");
+    scanf("%d%c%c", &age, &sex, &ms);
+    if((ms=='m') || (ms=='U' && sex == 'M' && age>30) || (ms=='U'&& sex=='F'&& age>25))
+    {
+        printf("Driver should be insured\n");
+    }
+    else
+    {
+        printf("Driver should not be insured\n");
+    }
+    return 0;
+}
